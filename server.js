cc4228bf82d19f379328c6701fff8b6769daec44
Durga Venkata Sai Maneeswar Mutyala <durga.mutyala@vedantu.com>
2022-06-22 14:13:53 +0530

    created server with multiple routes

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+  //   res.write('<p>Maneesh</p>');
+  //   res.write('<p>Maneesh</p>');
+  //   res.end();
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening on port 3000");
+});
