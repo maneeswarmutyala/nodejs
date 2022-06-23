e5805d231024d10beac93d73ffb6a693385260a6
Durga Venkata Sai Maneeswar Mutyala <durga.mutyala@vedantu.com>
2022-06-23 16:44:53 +0530

    mongoose routes

--- ejs/app.js
+++ ejs/app.js
@@ -1,30 +1,57 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const User = require("./models/user");
 const app = express();
+const DB_URI =
+  "mongodb+srv://<username>:<password>@nodejs.ej345.mongodb.net/?retryWrites=true&w=majority";
 
+//connecting and listening port
+mongoose
+  .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 //register view engine
-
 app.set("view engine", "ejs");
-//listening port
-app.listen(3000);
-//static files
-app.use(express.static('public'))
 
-//middleware
-app.use(morgan('dev'));
-app.use((req, res, next) => {
-  console.log('req url', req.url)
-  next();
+//static files
+app.use(express.static("public"));
+//adding data into mongodb
+app.get("/add-user", (req, res) => {
+  const user = new User({
+    username: "maneesh950",
+    name: "maneesh",
+  });
+  user.save().then((result) =>{
+    res.send(result);
+  }).catch(err => {
+    console.log(err)
+  })
+});
+//retrieving data from mongodb
+app.get("/all-users", (req, res) => {
+  User.find()
+  // .sort({ createdAt : -1}) // sortby fieldname
+  .then((result) =>{
+    res.render('index', {title: 'users', data: result});
+  }).catch(err => {
+    console.log(err)
+  })
 });
 
-
+//middleware
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log("req url", req.url);
+  next();
+});
 
 //routing
 app.get("/", (req, res) => {
   const data = [
-    { title: "hi", desc: "desc" },
-    { title: "hsfdsfi", desc: "descfsf" },
-    { title: "dfsdfshi", desc: "fsdfs" },
+    { username: "hi", desc: "desc" },
+    { username: "hsfdsfi", desc: "descfsf" },
+    { username: "dfsdfshi", desc: "fsdfs" },
   ];
   res.render("index", { title: "home", data });
 });
